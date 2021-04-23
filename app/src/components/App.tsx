9b1245b12277839004616f8f501bbbe68fc022f6
Daniele Gioria <daniele1gioria@gmail.com>
2021-04-23 17:08:32 +0200

    Upgraded App

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -6,7 +6,7 @@ import { ConversationsProvider } from '../contexts/ConversationsProvider';
 import { SocketProvider } from '../contexts/SocketProvider';
 
 function App() {
-  const [id, setId] = useLocalStorage('id');
+  const [id, setId] = useLocalStorage<string>('id', undefined);
 
   const dashboard = (
     <SocketProvider id={id}>
